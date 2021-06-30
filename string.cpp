8c2c268d7b0e59ba86d7a4adaf2a480d13d979fa
ShprotOFF <shprot1984@gmail.com>
2021-07-01 06:53:06 +1000

    Learning_strings

---
+++ string.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+	string myName;
+	cout << "Как тебя зовут...?\n";
+	cin >> myName;
+	cout << "Здарова " << myName << "!" <<endl;
+	return 0;
+}
