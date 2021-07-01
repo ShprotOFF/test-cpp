6a0e82ea139a476e9d76cf931ba5a2af340ae6c2
ShprotOFF <shprot1984@gmail.com>
2021-07-01 10:39:44 +1000

    Practic

--- test.cpp
+++ test.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include <string.h>
+using namespace std;
 
 int main(){
-	std::cout << "Хэллоу, мать его Ворлд...! =)" << std::endl;
+	
+	cout << "Привет...!" << endl;
+	cin.get();
+	cout << "Тебя как звать-то...?" << endl;
+
+	string yourName;
+	cin >> yourName;
+
+	cout << "А лет тебе сколько...?" << endl;
+	
+	int eath;
+	cin >> eath;
+
+	string text, text1, text2;
+	text = "О-о-о...! Ебать мой хуй! Так тебя звать ";
+	text1 = " и тебе ";
+	text2 = text + yourName + text1;
+
+	cout << text2 << eath << " лет...=)" << endl;
+
 	return 0;
 }
