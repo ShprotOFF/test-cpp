0a4729291fde1da4390721d0bb5b31bdafd8dae2
ShprotOFF <shprot1984@gmail.com>
2021-07-02 17:03:52 +1000

    Understanding_switch

---
+++ switch.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void playgame()
+{}
+
+void loadgame()
+{}
+
+void playmultiplayer()
+{}
+
+int main()
+{
+	int input;
+	
+	cout << "1. Play game\n";
+	cout << "2. Load game\n";
+	cout << "3. Play multiplayer\n";
+	cout << "4. Exit\n";
+	cout << "Selection: ";
+
+	cin >> input;
+	switch(input)
+	{
+		case 1:
+		playgame();
+		break;
+
+		case 2:
+		loadgame();
+		break;
+
+		case 3:
+		playmultiplayer();
+		break;
+
+		case 4:
+		cout << "Спасибо за игру\n";
+		break;
+
+		default:
+		cout << "Вы ничего не выбрали\n";
+		break;
+	}
+}
